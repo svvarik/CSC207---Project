6af8e725c9cfd3a2c8a40a699f70e5e230fbaa63
Sai Vinay V <sv.varikooty@mail.utoronto.ca>
2017-11-06 13:40:23 -0500

    Code affected: Tag.java Updated Tag java to include ArrayList of images it's used in, and changed constructor.

--- ProjectPhase1/src/Tag.java
+++ ProjectPhase1/src/Tag.java
@@ -1,13 +1,24 @@
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
 /**
  * This class represents the Tag that can be added to Image filenames, in order
  * to sort through and categorize images.
  */
 public class Tag {
 
+    // List of all images that this tag is used in
+    ArrayList<ImageFile> imagesAssociated;
+
     private String tag;
 
     public Tag(String tagName) {
-        this.tag = tagName;
+        if (Pattern.matches("^[@][a-zA-Z_0-9]*", tagName)) {
+            this.tag = tagName;
+            TagManager.add_tag(this);
+        }
     }
 
     public String getTag() {
