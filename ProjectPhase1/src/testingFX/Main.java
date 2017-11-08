0df92e2b7b73dd302b2f9bc7368f64515d96c71a
Sai Vinay V <sv.varikooty@mail.utoronto.ca>
2017-11-08 14:06:01 -0500

    IntellIJ issues, recommitting

--- ProjectPhase1/src/testingFX/Main.java
+++ ProjectPhase1/src/testingFX/Main.java
@@ -1,7 +1,6 @@
 package testingFX;
 
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -11,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("SelectDirectory.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
