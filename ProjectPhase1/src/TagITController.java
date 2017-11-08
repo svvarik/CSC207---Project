a2776963c713ae32d7f3c7b87d30a4a246315b98
Runtian <bettyruntian.wang@mail.utoronto.ca>
2017-11-08 15:53:21 -0500

    Added methods to TagITController.java to select directory and preview images in the directory.

--- ProjectPhase1/src/TagITController.java
+++ ProjectPhase1/src/TagITController.java
@@ -1,9 +1,100 @@
-import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.image.Image;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
-public class TagITController{
+import javax.swing.*;
+import javax.swing.text.html.ImageView;
+import java.io.File;
+import java.io.FileFilter;
+import java.io.FilenameFilter;
+import java.io.IOException;
 
+public class TagITController {
+    @FXML
+    public Button selectDir;
+
+    @FXML
+    public ListView<String> dirImages;
+
+    //Preview? then another button with "Select Image", open new scene with tagging
+    @FXML
+    public Button submit;
+
+    @FXML
+    public javafx.scene.image.ImageView imageView;
+
+    @FXML
+    public Button selectImage;
+
+    @FXML
+    public javafx.scene.image.ImageView imageToTag;
+
+    public void selectDirAction(ActionEvent event) {
+        DirectoryChooser fl = new DirectoryChooser();
+        File dir = fl.showDialog(null);
+
+//        FileChooser fc = new FileChooser();
+//        File dir = fc.showOpenDialog(null);
+
+        if (dir != null) {
+            File[] jpgF = dir.listFiles(new FileFilter() {
+                @Override
+                public boolean accept(File pathname) {
+                    return (pathname.isDirectory() || pathname.toString().endsWith(".jpg"));
+                }
+            });
+
+//            File[] jpgF = dir.listFiles(new FilenameFilter() {
+//                @Override
+//                public boolean accept(File dir, String name) {
+//                    return (name.toLowerCase().endsWith(".jpg"));
+//                }
+//            });
+            for (File f : jpgF) {
+                dirImages.getItems().add(f.getAbsolutePath());
+            }
+        }
+        dirImages.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+    }
+
+    public void previewImageAction(ActionEvent event) {
+        String imagePath = dirImages.getSelectionModel().getSelectedItem();
+        File f = new File(imagePath);
+        if (f.isDirectory()) {
+            // say something like "Please select an image."
+        } else {
+            Image image = new Image(f.toURI().toString());
+            imageView.setImage(image);
+        }
+    }
+
+    public void selectImageAction(ActionEvent event) throws IOException{
+        String imagePath = dirImages.getSelectionModel().getSelectedItem();
+        File f = new File(imagePath);
+        if (f.isDirectory()) {
+            // say smt like pls select an image.
+        } else {
+            Parent root2 = FXMLLoader.load(getClass().getResource("scene2.fxml"));
+            Image image = new Image(f.toURI().toString());
+            imageToTag.setImage(image);
+            Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+            Scene scene2 = new Scene(root2);
+            window.setScene(scene2);
+        }
+    }
+
+    // Go to photo. Start new scene- create imageFile in this step- associate with path?
+    // Tags...
 }
+
