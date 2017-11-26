6ed751921ef2e2247fd33930a298b5b8b4fb8a39
Arshia Mahmoodi <arshia.mahmoodi@mail.utoronto.ca>
2017-11-25 21:41:10 -0500

    Made it optional for the user to type in the @ sign (Adding @tag and tag is equivalent). Updated the innit method in Tag.java

--- phase2/ProjectPhase2/src/Tag.java
+++ phase2/ProjectPhase2/src/Tag.java
@@ -16,8 +16,13 @@ public class Tag implements Serializable {
      * @param tagName The name of the tag
      */
     public Tag(String tagName, TagManager tagManager) {
-        if (Pattern.matches("^[a-zA-Z0-9]*", tagName)) {
-            this.tag = "@" + tagName;
+        if (Pattern.matches("^[@]?[a-zA-Z0-9]*", tagName)) {
+            if (tagName.contains("@")) {
+                this.tag = tagName;
+            }
+            else{
+                this.tag = "@" + tagName;
+            }
             tagManager.addTag(this);
         }
     }
